9ed67fe39155de4a43333187b8fc4bf72bad721d
deept <deeptiravishankar92@gmail.com>
2025-07-30 13:30:17 +0530

    Day 2

--- script.js
+++ script.js
@@ -28,5 +28,58 @@ document.write("multiplcation:"+multiplcation+"<br>")
 let division = num1/num2
 document.write("division:"+division+"<br>")
 
+let myarray = ["Raj", "Manish", "Harshini", "Rakesh", "Manju"]
+// let myname1 = "Raj"
+// let myname2 = "Manish"
+// let myname3 = "Harshini"
+// let myname4 = "Manjur"
+//Its a tedious way to declare each variable to array members.
+//array is used when the information is scattered and no grouping is done.
+console.log(myarray[2])
 
 
+let myobj= {
+    name:"Rajdeep", 
+    age:30, 
+    education:"BCA", 
+    exp:15}
+//object is used when data is linked to each other. When there is a group of information
+console.log(myobj.age,myobj.name)    //to access the object data
+
+function dish(){
+    console.log("Step1: puri of tomato and onion")
+    console.log("step2: Egg")
+    console.log("Step3: Boil")
+    console.log("Step4: Tadka")
+}
+dish() //to display all steps.
+
+//to make them dynamic 
+
+function dish(ingredient){
+    console.log("Step1: puri of tomato and onion")
+    console.log("step2: "+ingredient)
+    console.log("Step3: Boil")
+    console.log("Step4: Tadka")
+}
+dish("potato") 
+dish("Egg")
+
+function login(id,password){
+    console.log("id:"+id)
+    console.log("Password:"+password)
+}
+login(101,2003)
+
+let deepti = {
+    maths: 98,
+    science: 78,
+    english: 90,
+    history: 89,
+    geography:100
+}
+function totalMarks(student){
+    let total = student.english+student.geography+student.history+student.maths+student.science
+    console.log(total)
+}
+totalMarks(deepti)
---
+++ todo.js
@@ -0,0 +1,10 @@
+let todo_list1 = ["Learn JS","do assignments", "Take Break"]
+let todo_list2 = ["Buy lipgloss", "Window shopping", "Chill after shopping in a cafe"]
+
+function print_list(list){
+    list.forEach(todo_list1 =>{
+        console.log(todo_list1);
+    });
+}
+print_list(todo_list1)
+print_list(todo_list2)
